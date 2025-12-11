bf8b519f845a9da77ae4966490aae6152eb570e0
keroppiii <fatimahnurmawati143@gmail.com>
2025-12-11 17:46:50 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'activity_tracker_ionic_H1D023019',
+  webDir: 'dist'
+};
+
+export default config;
